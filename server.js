9b0d1da723e0e688f687862f14d3302653a82fc6
npv34 <phanvanluan611996@gmail.com>
2022-05-30 11:40:03 +0700

    fix url

--- server.js
+++ server.js
@@ -3,35 +3,23 @@ let http = require('http');
 
 //b1 tao server
 let server = http.createServer(((req, res) => {
-
-    let urlArr = ['/search', '/'];
-
     // lấy url của request gửi lên server
     let urlRequest = req.url;
 
-    let checkUrl = urlArr.filter(item => {
-        return item === urlRequest
-    })
-
-    if (checkUrl.length === 0) {
-        // đọc file
-        fs.readFile('404.html', 'utf8', ((err, data) => {
-            if (err) {
-                throw Error('Error')
-            }
-            // set header tra ve dang Html
-            res.writeHead(200, {'content-type': 'text/html'})
-
-            // trả về response cho trình duyệt
-            return res.end(data)
-        }))
+    switch (urlRequest) {
+        case '/':
+            getTemplate('index', req, res);
+            break;
+        case '/search':
+            getTemplate('search', req, res);
+            break;
+        default:
+            getTemplate('404', req, res)
     }
+}))
 
-    if (req.url === '/search' && req.method === 'POST') {
-        console.log('form submited!')
-        return res.end()
-    }
-    fs.readFile('index.html', 'utf8', ((err, data) => {
+function getTemplate(templatePath, req, res) {
+    fs.readFile(`${templatePath}.html`, 'utf8', ((err, data) => {
         if (err) {
             throw Error('Error')
         }
@@ -39,8 +27,7 @@ let server = http.createServer(((req, res) => {
         res.writeHead(200, {'content-type': 'text/html'})
         return res.end(data)
     }))
-}))
-
+}
 server.listen('8080', function () {
     console.log('server running in port 8080')
 })
